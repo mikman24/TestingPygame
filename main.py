8226e053782c81484f60bed02aae96c7200be1fa
mkc <40498101+mikman24@users.noreply.github.com>
2023-04-05 12:40:24 +0200

    Player falls when hitting top/bottom
    Not working yet

--- main.py
+++ main.py
@@ -1,22 +1,82 @@
-import pygame
+# Testing out pygame by making a clone of the famous Flappy Bird game.
+import pygame, os, random
 from settings import *
+from sprites import *
 
 class Game:
     def __init__(self):
-        # Initialize game window, etc
+        # Initialize game
         pygame.init()
         pygame.mixer.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.icon = pygame.display.set_icon()
+        self.homeDir = os.getcwd()
+        self.imgDir = os.path.join(self.homeDir, "img")
+        self.sndDir = os.path.join(self.homeDir, "snd")
+        self.icon = pygame.image.load(os.path.join(self.imgDir, ICON)).convert()
+        self.icon.set_colorkey(BLACK)
+        pygame.display.set_icon(self.icon)
         pygame.display.set_caption(TITLE)
         self.clock = pygame.time.Clock()
         self.running = True
+        self.loadAssets()
 
     def new(self):
-        pass
+        # Start new game
+        self.allSprites = pygame.sprite.Group()
+        self.obstacles = pygame.sprite.Group()
+        self.player = Player(self)
+        self.allSprites.add(self.player)
+        self.obstacleBottom = Obstacle(self, 40, 300, False)
+        self.obstacles.add(self.obstacleBottom)
+        self.obstacleTop = Obstacle(self, WIDTH-100, -300, True)
+        self.obstacles.add(self.obstacleTop)
+        self.run()
+
+    def run(self):
+        # Game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
 
     def update(self):
-        pass
+        # Update game loop
+        self.allSprites.update()
+        self.obstacles.update()
+        # Die if player hits bottom of screen
+        if self.player.rect.bottom >= HEIGHT or self.player.rect.top <= 0:
+            self.player.gotHit()
+
+    def events(self):
+        # Process player input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    if self.playing:
+                        self.playing = False
+                    self.running = False
+                if event.key == pygame.K_SPACE:
+                    self.player.flap()
+
+    def draw(self):
+        # Update all drawings
+        self.screen.fill(BACKGROUND_COLOR)
+        self.allSprites.draw(self.screen)
+        pygame.display.flip()
+
+    def loadAssets(self):
+        # Load spritesheet
+        self.spritesheet = Spritesheet(os.path.join(self.imgDir, SPRITESHEET))
+        # Load sounds
+        self.flapSound = pygame.mixer.Sound(os.path.join(self.sndDir, SOUND_FLAP))
+        self.pointSound = pygame.mixer.Sound(os.path.join(self.sndDir, SOUND_POINT))
+        self.crashSound = pygame.mixer.Sound(os.path.join(self.sndDir, SOUND_CRASH))
 
 g = Game()
 while g.running:
---
+++ sprites.py
@@ -0,0 +1,68 @@
+# Sprite classes for Flappy bird clone
+import pygame, random
+from settings import *
+vec = pygame.math.Vector2
+
+class Spritesheet:
+    # Utility class for loading and parsing spritesheets
+    def __init__(self, filename):
+        self.spritesheet = pygame.image.load(filename).convert()
+
+    def getImage(self, x, y, width, height):
+        # Grab an image out of a larger spreadsheet
+        image = pygame.Surface((width, height))
+        image.blit(self.spritesheet, (0,0), (x, y, width, height))
+        image = pygame.transform.scale(image, (width*SCALE, height*SCALE))
+        return image
+    
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game):
+        self._layer = PLAYER_LAYER
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = self.game.spritesheet.getImage(0, 0, 32, 24)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.flapping = False
+        self.gameover = False
+
+    def update(self):
+        self.acc = vec(0, PLAYER_GRAVITY)        
+        self.vel += self.acc
+        self.pos += self.vel + 0.5*self.acc
+        self.rect.center = self.pos
+        if self.gameover:
+            if not self.pos.y < HEIGHT:
+                self.pos = vec(WIDTH/2, HEIGHT - self.image.get_height())
+                self.rect.center = self.pos
+                
+
+    def flap(self):
+        if not self.gameover:
+            self.flapping = True
+            self.vel.y = -PLAYER_FLAP
+
+    def gotHit(self):
+        self.gameover = True
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, isTop):
+        self._layer = OBSTACLE_LAYER
+        self.groups = game.allSprites, game.obstacles
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = self.game.spritesheet.getImage(32, 0, 40, 300)
+        self.image.set_colorkey(BLACK)
+        if isTop:
+            self.image = pygame.transform.flip(self.image, True, True)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pass
